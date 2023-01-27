9a4e848836f1b01f21d2af0065d1025f0de7f0fc
Rodo <rodonguyendd@gmail.com>
2023-01-27 23:45:25 +1000

    Union Find

---
+++ README.md
@@ -0,0 +1,3 @@
+# My data structures
+
+More: https://xlinux.nist.gov/dads/HTML/MerkleTree.html
--- draft.py
+++ draft.py
@@ -1,29 +1,7 @@
 import math
 
-
-def rob(nums) -> int:
-        curr, prev1, prev2 = 0, 0, 0
-        for i in nums:
-            curr = max(i + prev2, prev1)
-            prev1, prev2 = curr, prev1
-            print(curr, prev1, prev2)
-
-        return curr
-
-
-# nums = [2,7,9,3,1]
-# print(rob(nums), 12)
-# nums = [2,7,9,3,1,0]
-# print(rob(nums), 12)
-# nums = [1,2,3,1]
-# print(rob(nums), 4)
-# nums = [11]
-# print(rob(nums), 11)
-# nums = [3,1,1,3,3,2]
-# print(rob(nums), 8)
-# nums = [3,1,1,3]
-# print(rob(nums), 6)
-# nums = [1,3,10,1,1,3]
-# print(rob(nums), 14)
-
-print('123456'[1::-1])
+isConnected =[
+    [1,0,0,1],
+    [0,1,1,0],
+    [0,1,1,1],
+    [1,0,1,1]]
