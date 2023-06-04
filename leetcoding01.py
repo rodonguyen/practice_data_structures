18aea454821c3316e885a616a7170647d92ed83b
Rodo <rodonguyendd@gmail.com>
2023-06-04 12:20:11 +1000

    AerVision Interview

--- leetcoding01.py
+++ leetcoding01.py
@@ -3,35 +3,32 @@ import collections
 import heapq
 
 
+class UndergroundSystem:
 
-def find(x, root):
-   return root[x]
+    def __init__(self):
+        self.averageTime = dict()    # (start,end) -> [averageTime, number of trips]
+        self.ongoingTravel = dict()  # id -> [startStation, timestamp]
 
-def union(rootX, rootY, root):
-        for i in root.keys():
-            if root[i] == rootY:
-               root[i] = rootX
+    def checkIn(self, id: int, stationName: str, t: int) -> None:
+        self.ongoingTravel[id] = [stationName, t]
 
-def findRedundantConnection( edges) :
-        root = defaultdict(int)
+    def checkOut(self, id: int, stationName: str, t: int) -> None:
+        startStation, startTime = self.ongoingTravel[id]
         
-        for x,y in edges:
-            rootX = find(x, root)
-            rootY = find(y, root)
-            if rootX == 0 and rootY == 0:
-                root[x] = x
-                root[y] = x
-            elif rootX == 0:
-                root[x] = rootY
-            elif rootY == 0:
-                root[y] = rootX
-            elif rootX == rootY:
-                return [x,y]
-            else:
-                union(rootX, rootY, root)
-                
-            print(root)
+        # not the first record of this trip
+        if (startStation, stationName) in self.averageTime:
+            averageTime, numberOfTrips = self.averageTime[(startStation, stationName)]
+            self.averageTime[(startStation, stationName)][0] = \
+                (averageTime*numberOfTrips + (t-startTime)) / (numberOfTrips+1)
+            self.averageTime[(startStation, stationName)][1] += 1
+            del self.ongoingTravel[id]
+            return None
+        
+        # first record of this trip
+        self.averageTime[(startStation, stationName)] = [t-startTime, 1]
+        del self.ongoingTravel[id]
+        return None
 
-print(findRedundantConnection([[20,24],[3,17],[17,20],[8,15],[14,17],[6,17],[6,24],[13,19],[15,18],[1,9],[4,9],[4,19],[5,10],[4,21],[4,12],[5,6]]))
+    def getAverageTime(self, startStation: str, endStation: str) -> float:
+        return self.averageTime[(startStation, endStation)]
 
-# print(findRedundantConnection([[1,4],[3,4],[1,3],[1,2],[4,5]]))
